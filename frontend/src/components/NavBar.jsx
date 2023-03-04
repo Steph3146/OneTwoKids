a85763b888a0fc9118f7257193b5e8958f35a756
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2023-03-04 11:10:36 +0100

    Ajout du logo de l'app

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "../index.css";
+import logoOneTwoKids from "@assets/onetwokids_images/logoApp/logoOneTwoKids.png";
 
 export default function NavBar() {
   return (
     <nav className="flex flex-col bg-white border-gray-200 md:w-1/5 bg-gray-800 h-screen overflow-visible">
       <div className="flex flex-col items-center mt-4 text-white">
-        <p className="m-20">Logo One Two Kids</p>
+        <img src={logoOneTwoKids} alt="Logo One Two Kids" />
         <p className="mb-8 cursor-pointer hover:text-red-400">Se connecter</p>
         <p className="mb-8 cursor-pointer hover:text-red-400">
           Créer un compte
